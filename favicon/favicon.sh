54f41055e221aa2e5468667ececb4d9c305ec9d8
Daniel Micay <danielmicay@gmail.com>
2018-08-16 14:49:03 -0400

    add favicon generation script

---
+++ favicon/favicon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for size in 16 24 32 48 64; do
+  inkscape --export-area-drawing -z -e favicon_$size.png -w $size -h $size vector.svg
+done
+convert favicon_{16,24,32,48,64}.png favicon.ico
