c077c2640b65c61c183afaefa5dd7afa858c0740
Daniel Micay <danielmicay@gmail.com>
2019-05-08 13:45:42 -0400

    export-area-page instead of export-area-drawing

--- favicon/favicon.sh
+++ favicon/favicon.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 for size in 16 24 32 48 64; do
-  inkscape --export-area-drawing -z -e favicon_$size.png -w $size -h $size vector.svg
+  inkscape --export-area-page -z -e favicon_$size.png -w $size -h $size vector.svg
 done
 convert favicon_{16,24,32,48,64}.png favicon.ico
